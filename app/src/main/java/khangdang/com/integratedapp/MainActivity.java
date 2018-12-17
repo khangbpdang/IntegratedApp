e989862230df58375b39aeda047571ed19e484aa
khangdang <khangdang@mail.adephi.edu>
2018-12-16 20:37:51 -0500

    MainActivity redesign

--- app/src/main/java/khangdang/com/integratedapp/MainActivity.java
+++ app/src/main/java/khangdang/com/integratedapp/MainActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.Button;
+import android.widget.TextView;
 
 import khangdang.com.integratedapp.Calendar.CalendarMainActivity;
 import khangdang.com.integratedapp.YoutubeVideo.YouTubeActivity;
@@ -12,13 +15,22 @@ import khangdang.com.integratedapp.YoutubeVideo.YouTubeActivity;
 public class MainActivity extends AppCompatActivity {
     private Button calendarButton;
     private Button videoButton;
+    private TextView appName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        appName = (TextView) findViewById(R.id.app_name);
         calendarButton = (Button) findViewById(R.id.calendar);
         videoButton = (Button) findViewById(R.id.youtubeVideo);
+
+        Animation anim = new AlphaAnimation(0.0f, 1.0f);
+        anim.setDuration(1000); //You can manage the blinking time with this parameter
+        anim.setStartOffset(20);
+        anim.setRepeatMode(Animation.REVERSE);
+        anim.setRepeatCount(Animation.INFINITE);
+        appName.startAnimation(anim);
         calendarButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
